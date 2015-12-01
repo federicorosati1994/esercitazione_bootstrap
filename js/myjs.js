27d59c045ea4527d0cab945f400c74f6fd1f8e5e
federicorosati1994 <federicorosati1994@hotmail.it>
2015-12-01 01:33:40 +0100

    My first template one page _ Portfolio

--- js/myjs.js
+++ js/myjs.js
@@ -1,1 +1,29 @@
 console.log("ciao")
+
+$( ".lavoro1" ).click(function() {
+  $( ".item1" ).addClass( "active" );
+  $( ".item2" ).removeClass( "active" );
+  $( ".item3" ).removeClass( "active" );
+  $( ".item4" ).removeClass( "active" );
+});
+
+$( ".lavoro2" ).click(function() {
+  $( ".item2" ).addClass( "active" );
+  $( ".item1" ).removeClass( "active" );
+  $( ".item3" ).removeClass( "active" );
+  $( ".item4" ).removeClass( "active" );
+});
+
+$( ".lavoro3" ).click(function() {
+  $( ".item3" ).addClass( "active" );
+  $( ".item1" ).removeClass( "active" );
+  $( ".item2" ).removeClass( "active" );
+  $( ".item4" ).removeClass( "active" );
+});
+
+$( ".lavoro4" ).click(function() {
+  $( ".item4" ).addClass( "active" );
+  $( ".item1" ).removeClass( "active" );
+  $( ".item2" ).removeClass( "active" );
+  $( ".item3" ).removeClass( "active" );
+});
